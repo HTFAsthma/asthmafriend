6466bb122f83d42486b0a2fcff56f87100d94b53
laaph <laaph@laaph.com>
2013-04-13 14:02:49 -0700

    Views with tab bar controllers

---
+++ AsthmaFriend/LPHInitialViewController.h
@@ -0,0 +1,13 @@
+//
+//  LPHInitialViewController.h
+//  AsthmaFriend
+//
+//  Created by stanza on 4/13/13.
+//  Copyright (c) 2013 Laaph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPHInitialViewController : UIViewController
+
+@end
---
+++ AsthmaFriend/LPHInitialViewController.m
@@ -0,0 +1,38 @@
+//
+//  LPHInitialViewController.m
+//  AsthmaFriend
+//
+//  Created by stanza on 4/13/13.
+//  Copyright (c) 2013 Laaph. All rights reserved.
+//
+
+#import "LPHInitialViewController.h"
+
+@interface LPHInitialViewController ()
+
+@end
+
+@implementation LPHInitialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
